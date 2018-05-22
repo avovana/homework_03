0fa6e2c6f41874f4784f35406aebc2f1bfed236e
Vladimir <vladimir.englishbox@gmail.com>
2018-05-22 08:31:24 +0400

    Fixed cmake

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -10,7 +10,7 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_suite)
+BOOST_AUTO_TEST_SUITE(tests)
 
 BOOST_AUTO_TEST_CASE(factorial_function)
 {
