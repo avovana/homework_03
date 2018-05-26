df170231b08f3f1821b8bc4d2168c0c540ebc9e5
avovana <vladimir.englishbox@gmail.com>
2018-05-26 15:33:18 +0300

    Refactored indents

--- src/allocator.h
+++ src/allocator.h
@@ -4,11 +4,11 @@
 template <typename T, size_t Size = 10>
 class Allocator
 {
-private:
+    private:
     std::unique_ptr<unsigned char []> data;
     std::size_t offset;  
-    
-public:    
+
+    public:    
     template <typename U, size_t> friend class Allocator;
 
     using value_type = T;
@@ -18,40 +18,40 @@ public:
     {
         //std::cout << "Allocator dtor: " << this << std::endl;
     };
-    
+
     template <class U> 
-    struct rebind {
-        using other =  Allocator<U, Size>; 
-    };
-    
+        struct rebind {
+            using other =  Allocator<U, Size>; 
+        };
+
     Allocator()
-    : data( std::make_unique<unsigned char[]>(sizeof(value_type) * Size) )
-    , offset(0)
+        : data( std::make_unique<unsigned char[]>(sizeof(value_type) * Size) )
+        , offset(0)
     {
         //std::cout << "Allocator default ctor: " << this << std::endl;
     }
 
-	Allocator(const Allocator& rhs)
-		: data(nullptr)
-		, offset(0)
-	{
-		//std::cout << "Allocator copy ctor: " << this << std::endl;
-	}
+    Allocator(const Allocator& rhs)
+        : data(nullptr)
+        , offset(0)
+    {
+        //std::cout << "Allocator copy ctor: " << this << std::endl;
+    }
 
     template <typename U>
     Allocator(const Allocator<U>& rhs)
-		: offset(rhs.offset)
-		, data{ nullptr }
+        : offset(rhs.offset)
+        , data{ nullptr }
     { 
 
     }
 
     pointer allocate(std::size_t n)
     {
-		if (data == nullptr)
-		{
-			data = std::move(std::make_unique<unsigned char[]>(sizeof(value_type) * Size));
-		}
+        if (data == nullptr)
+        {
+            data = std::move(std::make_unique<unsigned char[]>(sizeof(value_type) * Size));
+        }
 
         if (offset + sizeof(value_type) * n > Size * sizeof(value_type))
         {
@@ -60,7 +60,7 @@ public:
 
         void * result = data.get() + offset;
         offset += sizeof(value_type) * n;
-        
+
         return static_cast<pointer>(result);
     }
 
@@ -70,7 +70,8 @@ public:
     }
 
     template <typename ... Args >
-    void construct(T* p, Args&& ... args) {
+    void construct(T* p, Args&& ... args) 
+    {
         new(p) T(std::forward <Args>(args) ... );
     }
 };
